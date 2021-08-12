55d0cd4f6b814e79a831e617791f8579d0091765
YousefZidan3 <87281005+YousefZidan3@users.noreply.github.com>
2021-08-12 19:47:53 +0200

    Add files via upload

---
+++ DAY 7- CLASSCODE JAVA.js
@@ -0,0 +1,7 @@
+window.alert("welcome to our website")
+document.getElementById('e1').innerHTML ="we school"
+document.getElementById('e2').innerHTML= "my school"
+function track() {document.getElementById('e3').innerHTML= 'futue career camp'}
+function today() {document.getElementById('e4').innerHTML=Date()
+
+}
